15bfa715c0c54ae541446f6e0eea6be5f17bfe91
VanoLetniy <126766793+VanoLetniy@users.noreply.github.com>
2023-06-30 23:07:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+tg_bot_token = '6397073673:AAEkMlkrD53MWPy5UJKgiqqV4Kfb-n4uQvs'  # Токен бота полученный ботом BotFather
+# Словарь с доступными валютами
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
