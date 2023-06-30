6a64283abda976732b00f69bbadfd43ab9c91517
VanoLetniy <126766793+VanoLetniy@users.noreply.github.com>
2023-06-30 23:07:01 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-tg_bot_token = '6397073673:AAEkMlkrD53MWPy5UJKgiqqV4Kfb-n4uQvs'  # Токен бота полученный ботом BotFather
-# Словарь с доступными валютами
-keys = {
-    'евро': 'EUR',
-    'доллар': 'USD',
-    'рубль': 'RUB',
-}
